a0424bcac09465e5bdacbf8583b28b68a2e29aaf
Nathan Chancellor <natechancellor@gmail.com>
2017-10-19 23:46:22 -0700

    staging: qcacld-3.0: Fix all relevant GCC warnings
    This is the third part in the GCC warnings series, fixing all
    warnings that come from CAF's Wi-Fi stack.
    
    This was done with the following toolchain version:
    aarch64-linux-gnu-gcc (Linaro GCC 7.2-2017.11) 7.2.1 20171011
    
    If a warning was a false positive, it was disabled in the folder's
    Makefile. I believe all of these are correct but I am 100% open to
    discussion (I am not infallible).
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/staging/qcacld-3.0/core/sme/src/csr/csr_neighbor_roam.c
+++ drivers/staging/qcacld-3.0/core/sme/src/csr/csr_neighbor_roam.c
@@ -1040,9 +1040,8 @@ static void csr_neighbor_roam_info_ctx_init(
 				qdf_mem_free(pMac->roam.pReassocResp);
 				pMac->roam.pReassocResp = NULL;
 			}
-		} else
+		} else {
 #endif
-
 			csr_roam_offload_scan(pMac, session_id,
 				ROAM_SCAN_OFFLOAD_START,
 				REASON_CTX_INIT);
@@ -1055,7 +1054,9 @@ static void csr_neighbor_roam_info_ctx_init(
 					ROAM_SCAN_OFFLOAD_STOP,
 					REASON_SUPPLICANT_DISABLED_ROAMING);
 			}
-
+#ifdef WLAN_FEATURE_ROAM_OFFLOAD
+		}
+#endif
 	}
 }
 
